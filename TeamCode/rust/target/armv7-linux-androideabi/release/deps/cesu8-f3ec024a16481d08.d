31ec43e39533ada1db122b162dab6cb1b90c53d7
InfinityDevTech <47366052+InfinityDevTech@users.noreply.github.com>
2023-09-13 21:01:12 -0400

    Fixed gitignore

--- TeamCode/rust/target/armv7-linux-androideabi/release/deps/cesu8-f3ec024a16481d08.d
+++
@@ -1,8 +0,0 @@
-/home/jwjoh/Desktop/FTC-PROJECTS/FtcRobotCOntroller/TeamCode/rust/target/armv7-linux-androideabi/release/deps/cesu8-f3ec024a16481d08.rmeta: /home/jwjoh/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cesu8-1.1.0/src/lib.rs /home/jwjoh/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cesu8-1.1.0/src/unicode.rs
-
-/home/jwjoh/Desktop/FTC-PROJECTS/FtcRobotCOntroller/TeamCode/rust/target/armv7-linux-androideabi/release/deps/libcesu8-f3ec024a16481d08.rlib: /home/jwjoh/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cesu8-1.1.0/src/lib.rs /home/jwjoh/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cesu8-1.1.0/src/unicode.rs
-
-/home/jwjoh/Desktop/FTC-PROJECTS/FtcRobotCOntroller/TeamCode/rust/target/armv7-linux-androideabi/release/deps/cesu8-f3ec024a16481d08.d: /home/jwjoh/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cesu8-1.1.0/src/lib.rs /home/jwjoh/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cesu8-1.1.0/src/unicode.rs
-
-/home/jwjoh/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cesu8-1.1.0/src/lib.rs:
-/home/jwjoh/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cesu8-1.1.0/src/unicode.rs:
--- TeamCode/rust/target/armv7-linux-androideabi/release/deps/rust_code.d
+++
@@ -1,9 +0,0 @@
-/home/jwjoh/Desktop/FTC-PROJECTS/FtcRobotCOntroller/TeamCode/rust/target/armv7-linux-androideabi/release/deps/librust_code.so: src/lib.rs src/threads/mod.rs src/threads/math.rs src/java_translations.rs src/tests.rs
-
-/home/jwjoh/Desktop/FTC-PROJECTS/FtcRobotCOntroller/TeamCode/rust/target/armv7-linux-androideabi/release/deps/rust_code.d: src/lib.rs src/threads/mod.rs src/threads/math.rs src/java_translations.rs src/tests.rs
-
-src/lib.rs:
-src/threads/mod.rs:
-src/threads/math.rs:
-src/java_translations.rs:
-src/tests.rs:
