723ced3add033f0736456b52f18131142e205322
InfinityDevTech <47366052+InfinityDevTech@users.noreply.github.com>
2023-09-13 21:00:04 -0400

    test

--- TeamCode/rust/target/aarch64-linux-android/release/deps/num_cpus-d6b315026da30412.d
+++ TeamCode/rust/target/aarch64-linux-android/release/deps/num_cpus-d6b315026da30412.d
@@ -1,7 +1,7 @@
-/home/jwjoh/Desktop/Free-Wifi-Projects/ftc-centerstage/TeamCode/rust/target/aarch64-linux-android/release/deps/num_cpus-d6b315026da30412.rmeta: /home/jwjoh/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num_cpus-1.16.0/src/lib.rs
+/home/jwjoh/Desktop/FTC-PROJECTS/FtcRobotCOntroller/TeamCode/rust/target/aarch64-linux-android/release/deps/num_cpus-d6b315026da30412.rmeta: /home/jwjoh/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num_cpus-1.16.0/src/lib.rs
 
-/home/jwjoh/Desktop/Free-Wifi-Projects/ftc-centerstage/TeamCode/rust/target/aarch64-linux-android/release/deps/libnum_cpus-d6b315026da30412.rlib: /home/jwjoh/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num_cpus-1.16.0/src/lib.rs
+/home/jwjoh/Desktop/FTC-PROJECTS/FtcRobotCOntroller/TeamCode/rust/target/aarch64-linux-android/release/deps/libnum_cpus-d6b315026da30412.rlib: /home/jwjoh/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num_cpus-1.16.0/src/lib.rs
 
-/home/jwjoh/Desktop/Free-Wifi-Projects/ftc-centerstage/TeamCode/rust/target/aarch64-linux-android/release/deps/num_cpus-d6b315026da30412.d: /home/jwjoh/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num_cpus-1.16.0/src/lib.rs
+/home/jwjoh/Desktop/FTC-PROJECTS/FtcRobotCOntroller/TeamCode/rust/target/aarch64-linux-android/release/deps/num_cpus-d6b315026da30412.d: /home/jwjoh/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num_cpus-1.16.0/src/lib.rs
 
 /home/jwjoh/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num_cpus-1.16.0/src/lib.rs:
--- TeamCode/rust/target/armv7-linux-androideabi/release/deps/rust_code.d
+++ TeamCode/rust/target/armv7-linux-androideabi/release/deps/rust_code.d
@@ -1,6 +1,6 @@
-/home/jwjoh/Desktop/Free-Wifi-Projects/ftc-centerstage/TeamCode/rust/target/armv7-linux-androideabi/release/deps/librust_code.so: src/lib.rs src/threads/mod.rs src/threads/math.rs src/java_translations.rs src/tests.rs
+/home/jwjoh/Desktop/FTC-PROJECTS/FtcRobotCOntroller/TeamCode/rust/target/armv7-linux-androideabi/release/deps/librust_code.so: src/lib.rs src/threads/mod.rs src/threads/math.rs src/java_translations.rs src/tests.rs
 
-/home/jwjoh/Desktop/Free-Wifi-Projects/ftc-centerstage/TeamCode/rust/target/armv7-linux-androideabi/release/deps/rust_code.d: src/lib.rs src/threads/mod.rs src/threads/math.rs src/java_translations.rs src/tests.rs
+/home/jwjoh/Desktop/FTC-PROJECTS/FtcRobotCOntroller/TeamCode/rust/target/armv7-linux-androideabi/release/deps/rust_code.d: src/lib.rs src/threads/mod.rs src/threads/math.rs src/java_translations.rs src/tests.rs
 
 src/lib.rs:
 src/threads/mod.rs:
