be22deecfcb186884ddffec366b86997b95772b1
InfinityDevTech <47366052+InfinityDevTech@users.noreply.github.com>
2023-10-13 20:37:24 -0400

    sync

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freeWifi/Constants.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freeWifi/Constants.kt
@@ -5,4 +5,7 @@ import com.acmerobotics.dashboard.config.Config
 @Config
 object Constants {
     @JvmField var Debug: Boolean = true;
+
+    // When you push button to change speed, how much to modify speed by per tick.
+    @JvmField var speed_modifier: Float = 0.05F;
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freeWifi/DriveModes/Mecanum.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freeWifi/DriveModes/Mecanum.kt
@@ -3,15 +3,15 @@ package org.firstinspires.ftc.teamcode.freeWifi.DriveModes
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.Gamepad
+import org.firstinspires.ftc.teamcode.freeWifi.Constants
 import org.firstinspires.ftc.teamcode.freeWifi.Robot.Motors
 import org.firstinspires.ftc.teamcode.freeWifi.Robot.Robot
+import kotlin.time.Duration.Companion.milliseconds
 
 @TeleOp(name = "Mecanum", group = "TelPrimary")
 class Mecanum : LinearOpMode() {
 
     private val robot = Robot(this);
-
-    private val move_speed = 1.0;
 
     // INIT
     override fun runOpMode() {
@@ -29,37 +29,54 @@ class Mecanum : LinearOpMode() {
         val gamepad2: Gamepad = this.gamepad2;
 
         var speed: Double = 1.0;
-        var lastLoopEndTime: Long = System.nanoTime();
+        var lastTime: Long = System.currentTimeMillis();
 
         while (opModeIsActive()) {
 
             // Delta Time is the time between loops
-            val deltaTime: Long = (System.nanoTime() - lastLoopEndTime) / 1000;
+            fun deltaTime(): Float {
+                val t = (lastTime - System.currentTimeMillis()).toFloat()
+                lastTime = System.currentTimeMillis()
+                return t
+            }
 
             // Per loop variable initialization
             val decreaseSpeed = gamepad1.left_bumper;
             val increaseSpeed = gamepad1.right_bumper;
+            telemetry.addData("Touchpad 1", gamepad1.touchpad)
+            telemetry.addData("Touchpad 2", gamepad1.touchpad_finger_1)
+            telemetry.addData("Touchpad 3", gamepad1.touchpad_finger_1_x)
+            telemetry.addData("Touchpad 3", gamepad1.touchpad_finger_1_y)
+
+            telemetry.addData("Current speed", speed);
+            telemetry.addData("Speed Change", (Constants.speed_modifier));
+            telemetry.addData("Speed Change", (Constants.speed_modifier * deltaTime()));
+            if (decreaseSpeed) {
+                if (speed - (Constants.speed_modifier * deltaTime()) < 0.0) {
+                    speed = 0.0;
+                } else {
+                    speed += (Constants.speed_modifier * deltaTime());
+                }
+            } else if (increaseSpeed) {
+                if (speed + (Constants.speed_modifier * deltaTime()) > 1.0) {
+                    speed = 1.0;
+                } else {
+                    speed -= (Constants.speed_modifier * deltaTime());
+                }
+            }
 
             // Assign movement controls
             val vertical = gamepad1.left_stick_y
-            val horizontal = -gamepad1.left_stick_x
-            val rotation = gamepad1.right_stick_x.toDouble()
+            val horizontal = -gamepad1.right_stick_x
+            val rotation = gamepad1.left_stick_x.toDouble()
 
             robot.setMotorPower(Motors.LeftFront, (-rotation + vertical + horizontal) * speed)
             robot.setMotorPower(Motors.RightFront, (rotation + vertical + -horizontal) * speed)
             robot.setMotorPower(Motors.LeftBack, (-rotation + vertical + -horizontal) * speed)
             robot.setMotorPower(Motors.RightBack, (rotation + vertical + horizontal) * speed)
 
-            telemetry.addData("Current speed", speed);
-            if (decreaseSpeed) {
-                speed -= (0.0001 * deltaTime)
-            } else if (increaseSpeed) {
-                speed += (0.00
-                01 * deltaTime)
-            }
 
-            telemetry.addData("Loop Time (ms)", deltaTime)
-            lastLoopEndTime = System.nanoTime();
+            telemetry.addData("Loop Time (ms)", deltaTime())
             telemetry.update();
         }
     }
